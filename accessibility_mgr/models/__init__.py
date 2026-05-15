dbcfe25fb92540ae522e7553fe3374db5f08528a
Ryan (Michael) Hunsaker <3277946+mrhunsaker@users.noreply.github.com>
2026-05-15 17:52:22 -0600

    Export legacy inventory models

--- accessibility_mgr/models/__init__.py
+++ accessibility_mgr/models/__init__.py
@@ -1,5 +1,14 @@
 """SQLAlchemy-era model package used by the documentation reference pages."""
 
 from .assets import Asset, AssetMetadata, Project, WorkflowEvent
+from .inventory import Category, InventoryItem, InventoryTransaction
 
-__all__ = ["Asset", "AssetMetadata", "Project", "WorkflowEvent"]
+__all__ = [
+    "Asset",
+    "AssetMetadata",
+    "Project",
+    "WorkflowEvent",
+    "Category",
+    "InventoryItem",
+    "InventoryTransaction",
+]
